bfb329849be91ffc7192961db008b3cf4272674d
bengoh815 <64095338+bengoh815@users.noreply.github.com>
2021-12-28 16:08:50 -0800

    Checkpoint
    Finsihed most tutorial left testing

--- README.md
+++
@@ -1,2 +0,0 @@
-# ISDH
- Document Handler Website
---
+++ server/app/config/db.config.js
@@ -0,0 +1,3 @@
+module.exports = {
+    url: "mongodb+srv://dbAdmin:ZCXTvz9QRKhRseA@cluster0.j1efn.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+  };
