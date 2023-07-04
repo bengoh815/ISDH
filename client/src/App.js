3829e796665b0a8ce612b67aaa60419c8e7a7a5d
bengoh815 <64095338+bengoh815@users.noreply.github.com>
2023-07-03 19:07:17 -0500

    functioning login signup

--- client/src/App.js
+++ client/src/App.js
@@ -11,12 +11,37 @@ import Home from "./components/Home";
 import Login from "./components/Login";
 import Signup from "./components/Signup";
 import GoogleSignIn from "./components/GoogleSignIn";
-import { Box, Stack, ThemeProvider } from "@mui/material";
+import {
+  AppBar,
+  Avatar,
+  Box,
+  Button,
+  Checkbox,
+  FormControl,
+  FormControlLabel,
+  Input,
+  InputLabel,
+  Paper,
+  Stack,
+  ThemeProvider,
+  Toolbar,
+  Typography,
+  styled,
+  useTheme,
+} from "@mui/material";
 import SideNav from "./components/SideNav";
 import { useState } from "react";
 import Mainbox from "./components/Mainbox";
+import Userbox from "./components/Userbox";
+import { LockOutlined } from "@mui/icons-material";
+import SpacingHeader from "./components/SpacingHeader";
+
+const StyledAppBar = styled(AppBar)(({ theme }) => ({
+  zIndex: theme.zIndex.drawer + 1,
+}));
 
 function App() {
+  const theme = useTheme();
   const [openDrawer, setOpenDrawer] = useState(false);
 
   const toggleDrawer = () => {
@@ -27,17 +52,33 @@ function App() {
     <>
       <GoogleOAuthProvider clientId={process.env.REACT_APP_G_CLIENT_ID}>
         {/* <ThemeProvider> */}
-        <Navbar toggleDrawer={toggleDrawer} />
-        <Stack direction="row" spacing={2} justifyContent="space-between">
-          <SideNav openDrawer={openDrawer} />
-          <Mainbox />
-        </Stack>
         {/* rest of the app */}
+        {/* Leave this for now */}
+        <StyledAppBar position="fixed">
+          <Toolbar
+            sx={{
+              display: "flex",
+              justifyContent: "space-between",
+            }}
+          >
+            <Box>empty</Box>
+            <Typography>Tally-ho</Typography>
+          </Toolbar>
+        </StyledAppBar>
+        <SpacingHeader />
+        <Signup />
+        <Login />
         {/* </ThemeProvider> */}
       </GoogleOAuthProvider>
     </>
   );
 }
+
+// const working = (        <Navbar toggleDrawer={toggleDrawer} />
+// <Stack direction="row" spacing={2} justifyContent="space-between">
+//   <SideNav openDrawer={openDrawer} />
+//   <Mainbox />
+// </Stack>);
 
 // const temp = (
 //   <GoogleOAuthProvider clientId={process.env.REACT_APP_G_CLIENT_ID}>
--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,3 +1,95 @@
+import { LockOutlined, Visibility, VisibilityOff } from "@mui/icons-material";
+import {
+  Avatar,
+  Box,
+  Button,
+  FormControl,
+  InputLabel,
+  FormGroup,
+  Input,
+  Paper,
+  Stack,
+  Typography,
+  useTheme,
+  IconButton,
+  InputAdornment,
+  Divider,
+} from "@mui/material";
+import { useState } from "react";
+import GoogleSignIn from "./GoogleSignIn";
+
+/*
+TODO
+  Fix google button width
+  submit functionality
+*/
+
 export default function Login() {
-  return <div>Login</div>;
+  const theme = useTheme();
+
+  const [showPassword, setShowPassword] = useState(false);
+
+  const handleClickShowPassword = () => setShowPassword(!showPassword);
+
+  return (
+    <>
+      <Box
+        sx={{ display: "flex", justifyContent: "center", marginTop: "80px" }}
+      >
+        <Paper sx={{ width: "40%" }}>
+          <Stack
+            direction="column"
+            sx={{ alignItems: "center", mt: "10px", mb: "20px" }}
+          >
+            <Avatar sx={{ bgcolor: "red" }}>
+              <LockOutlined />
+            </Avatar>
+            <Typography component="h1" variant="h5">
+              Log in
+            </Typography>
+            <FormGroup sx={{ width: "80%" }}>
+              <FormControl margin="normal" required fullWidth>
+                <InputLabel>Email Address</InputLabel>
+                <Input></Input>
+              </FormControl>
+              <FormControl margin="normal" required fullWidth>
+                <InputLabel htmlFor="standard-adornment-password">
+                  Password
+                </InputLabel>
+                <Input
+                  id="standard-adornment-password"
+                  type={showPassword ? "text" : "password"}
+                  endAdornment={
+                    <InputAdornment position="end">
+                      <IconButton
+                        aria-label="toggle password visibility"
+                        onClick={handleClickShowPassword}
+                        edge="end"
+                      >
+                        {showPassword ? <VisibilityOff /> : <Visibility />}
+                      </IconButton>
+                    </InputAdornment>
+                  }
+                  label="Password"
+                />
+              </FormControl>
+
+              <Button
+                type="submit"
+                fullWidth
+                variant="contained"
+                sx={{ marginTop: 3, marginBottom: 3 }}
+              >
+                Log in
+              </Button>
+            </FormGroup>
+            <Divider
+              sx={{ width: "80%", borderBottomWidth: 2, marginBottom: 3 }}
+            />
+            <GoogleSignIn />
+          </Stack>
+        </Paper>
+      </Box>
+    </>
+  );
 }
